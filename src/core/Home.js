2365318c36b0e1b1fef07a0b97bf163345e599c5
gagansaini1212 <gagansaini1212@gmail.com>
2022-07-11 11:04:28 +0530

    added product card

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,14 +1,13 @@
-import { API } from '../backend';
 import '../styles.css';
 import Base from './Base';
+import Card from './Card';
 
 const Home = () => {
-  console.log('API is', API);
   return (
     <Base title="Home Page" description="Welcome to the T-shirt stuff">
       <div className="row">
-        <div className="col-4">
-          <div className="btn btn-success">Test</div>
+        <div className="col-4 text-center">
+          <Card />
         </div>
         <div className="col-4">
           <div className="btn btn-success">Test</div>
