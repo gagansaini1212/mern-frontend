160ff390af81819b750d44521089bde54cf0b5fa
gagansaini1212 <gagansaini1212@gmail.com>
2022-07-18 14:27:41 +0530

    cart bug fixed

--- src/core/Cart.js
+++ src/core/Cart.js
@@ -1,11 +1,10 @@
-import { useState, useEffect } from 'react';
-
+import React, { useState, useEffect } from 'react';
 import '../styles.css';
+import { API } from '../backend';
 import Base from './Base';
 import Card from './Card';
 import { loadCart } from './helper/cartHelper';
 import PaymentB from './PaymentB';
-// import StripeCheckout from './StripeCheckout';
 
 const Cart = () => {
   const [products, setProducts] = useState([]);
@@ -18,20 +17,24 @@ const Cart = () => {
   const loadAllProducts = (products) => {
     return (
       <div>
-        <h2>This section to load products</h2>
-
-        {products?.map((product, index) => {
-          return (
-            <Card
-              key={index}
-              product={product}
-              addToCart={false}
-              removeFromCart={true}
-              setReload={setReload}
-              reload={reload}
-            />
-          );
-        })}
+        <h2>This section is to load products</h2>
+        {products?.map((product, index) => (
+          <Card
+            key={index}
+            product={product}
+            removeFromCart={true}
+            addtoCart={false}
+            setReload={setReload}
+            reload={reload}
+          />
+        ))}
+      </div>
+    );
+  };
+  const loadCheckout = () => {
+    return (
+      <div>
+        <h2>This section for checkout</h2>
       </div>
     );
   };
@@ -40,15 +43,12 @@ const Cart = () => {
     <Base title="Cart Page" description="Ready to checkout">
       <div className="row text-center">
         <div className="col-6">
-          {products?.length >= 0 ? (
+          {products?.length > 0 ? (
             loadAllProducts(products)
           ) : (
-            <h3>No products in cart</h3>
+            <h4>No products</h4>
           )}
         </div>
-        {/* <div className="col-6">
-          <StripeCheckout products={products} setReload={setReload} />
-        </div> */}
         <div className="col-6">
           <PaymentB products={products} setReload={setReload} />
         </div>
--- src/core/PaymentB.js
+++ src/core/PaymentB.js
@@ -1,62 +1,104 @@
 import React, { useState, useEffect } from 'react';
+import { loadCart, cartEmpty } from './helper/cartHelper';
 import { Link } from 'react-router-dom';
+import { getMeToken, processPayment } from './helper/paymentHelper';
+import { createOrder } from './helper/orderHelper';
+import { isAuthenticated } from '../auth/helper';
+
 import DropIn from 'braintree-web-drop-in-react';
 
-import { isAuthenticated } from '../auth/helper';
-import { cartEmpty, loadCart } from './helper/cartHelper';
-import { createOrder } from './helper/orderHelper';
-import { getMeToken, processPayment } from './helper/paymentHelper';
-
-const PaymentB = ({ products, setReload }) => {
+const PaymentB = ({ products, setReload = (f) => f, reload = undefined }) => {
   const [info, setInfo] = useState({
     loading: false,
     success: false,
-    error: '',
     clientToken: null,
+    error: '',
     instance: {},
   });
 
   const userId = isAuthenticated() && isAuthenticated().user._id;
   const token = isAuthenticated() && isAuthenticated().token;
 
-  const getToken = (uerId, token) => {
+  const getToken = (userId, token) => {
     getMeToken(userId, token).then((info) => {
+      // console.log("INFORMATION", info);
       if (info.error) {
         setInfo({ ...info, error: info.error });
       } else {
         const clientToken = info.clientToken;
-        setInfo(clientToken);
+        setInfo({ clientToken });
       }
     });
   };
-  useEffect(() => {
-    getToken(userId, token);
-  }, []);
 
-  const showDropIn = () => {
+  const showbtdropIn = () => {
     return (
       <div>
-        {/* {info.clientToken !== null && products?.length > 0 ? ( */}
-        <div>
-          <DropIn
-            options={{ authorization: info.clientToken }}
-            onInstance={(instance) => (info.instance = instance)}
-          />
-          <button className="btn btn-success btn-block" onClick={() => {}}>
-            Buy
-          </button>
-        </div>
-        {/* ) : (
-          <h3>Please login or ad something to cart</h3>
-        )} */}
+        {info.clientToken !== null && products.length > 0 ? (
+          <div>
+            <DropIn
+              options={{ authorization: info.clientToken }}
+              onInstance={(instance) => (info.instance = instance)}
+            />
+            <button className="btn btn-block btn-success" onClick={onPurchase}>
+              Buy
+            </button>
+          </div>
+        ) : (
+          <h3>Please login or add something to cart</h3>
+        )}
       </div>
     );
   };
 
+  useEffect(() => {
+    getToken(userId, token);
+  }, []);
+
+  const onPurchase = () => {
+    setInfo({ loading: true });
+    let nonce;
+    let getNonce = info.instance.requestPaymentMethod().then((data) => {
+      nonce = data.nonce;
+      const paymentData = {
+        paymentMethodNonce: nonce,
+        amount: getAmount(),
+      };
+      processPayment(userId, token, paymentData)
+        .then((response) => {
+          setInfo({ ...info, success: response.success, loading: false });
+          console.log('PAYMENT SUCCESS');
+          const orderData = {
+            products: products,
+            transaction_id: response.transaction.id,
+            amount: response.transaction.amount,
+          };
+          createOrder(userId, token, orderData);
+          cartEmpty(() => {
+            console.log('Did we got a crash?');
+          });
+
+          setReload(!reload);
+        })
+        .catch((error) => {
+          setInfo({ loading: false, success: false });
+          console.log('PAYMENT FAILED');
+        });
+    });
+  };
+
+  const getAmount = () => {
+    let amount = 0;
+    products.map((p) => {
+      amount = amount + p.price;
+    });
+    return amount;
+  };
+
   return (
     <div>
-      <h3>test BT</h3>
-      {showDropIn()}
+      <h3>Your bill is {getAmount()} $</h3>
+      {showbtdropIn()}
     </div>
   );
 };
